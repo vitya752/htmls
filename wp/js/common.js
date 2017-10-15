46fda4d6e9cca99ce9da0945972dc51638cc7ff4
vitya752 <vitya752@ukr.net>
2017-10-15 19:01:09 +0300

    w

---
+++ wp/js/common.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+	$(window).load(function() { 
+		$(".loader-inner").fadeOut(); 
+		$(".loader").delay(400).fadeOut("slow"); 
+	});
+
+	function heightDetect() {
+		$("header").css("height", $(window).height());
+	};
+	heightDetect();
+	$(window).resize(function() {
+		heightDetect();
+	});
+
+	$(".btn-menu, .menu_item").click(function() {
+		if ($(".top-mnu").is(":visible")) {
+			$(".sandwich").removeClass("active");
+			$(".top-text").css("opacity", "1");
+			$(".top-mnu").fadeOut(600);
+			$(".top-mnu li a").removeClass("fadeInUp animated");
+		} else {
+			$(".top-text").css("opacity", ".1");
+			$(".sandwich").addClass("active");
+			$(".top-mnu").fadeIn(600);
+			$(".top-mnu li a").addClass("fadeInUp animated");
+		};
+	});
+	
+});
