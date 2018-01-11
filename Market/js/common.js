be99d296f955629cf5b0ddc97bdcbc234383f64d
vitya752 <vitya752@ukr.net>
2018-01-11 23:40:18 +0200

    й

--- Market/js/common.js
+++ Market/js/common.js
@@ -1,10 +1,16 @@
 $(function() {
 
-	  $('.slideshow').slick({
-	  	speed: 1200,
-	  	prevArrow: '<button type="button" class="prev"><img src="img/prev.png"></button>',
-		nextArrow: '<button type="button" class="next"><img src="img/next.png"></button>'
-	  });
+	$('.slideshow').slick({
+		speed: 1200,
+		prevArrow: '<button type="button" class="prev"><img src="img/prev.png"></button>',
+	nextArrow: '<button type="button" class="next"><img src="img/next.png"></button>'
+	});
 
+  	$(".special_products .filter_switch_div li").click(function() {
+		$(".special_products .filter_switch_div li").removeClass("active");
+		$(this).addClass("active");
+	});
+
+	$("#grid_spec_products").mixItUp();
 
 });
