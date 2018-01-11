6a49a722601a6489a7bbda92ed2821de879368f4
vitya752 <vitya752@ukr.net>
2018-01-11 18:44:18 +0200

    ww

---
+++ Market/js/common.js
@@ -0,0 +1,10 @@
+$(function() {
+
+	  $('.slideshow').slick({
+	  	speed: 1200,
+	  	prevArrow: '<button type="button" class="prev"><img src="img/prev.png"></button>',
+		nextArrow: '<button type="button" class="next"><img src="img/next.png"></button>'
+	  });
+
+
+});
