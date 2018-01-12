e6e8f45e1e01bef2af1e337ad1522db239792762
vitya752 <vitya752@ukr.net>
2018-01-12 19:29:10 +0200

    d

--- Market/js/common.js
+++ Market/js/common.js
@@ -2,15 +2,80 @@ $(function() {
 
 	$('.slideshow').slick({
 		speed: 1200,
-		prevArrow: '<button type="button" class="prev"><img src="img/prev.png"></button>',
-	nextArrow: '<button type="button" class="next"><img src="img/next.png"></button>'
+		touchMove: false,
+		swipeToSlide: false
 	});
 
+	$('.new_products_section .products_wrap').slick({
+		speed: 1200,
+		touchMove: false,
+		swipeToSlide: false,
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		responsive: [
+		{
+		  breakpoint: 1024,
+		  settings: {
+		    slidesToShow: 3,
+		    slidesToScroll: 3,
+		    infinite: true,
+		    dots: true
+		  }
+		},
+	    {
+	      breakpoint: 600,
+	      settings: {
+	        slidesToShow: 2,
+	        slidesToScroll: 2
+	      }
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	        slidesToShow: 1,
+	        slidesToScroll: 1
+	      }
+	    }
+	  ]
+	});
+/**
+*Custom buttons for the change slide.
+**/
+	$('.prev').on('click', function() {
+	  $('.slideshow').slick('slickPrev');
+	});
+	$('.next').on('click', function() {
+	  $('.slideshow').slick('slickNext');
+	});
+
+	$('.new_products_section .prev').on('click', function() {
+	  $('.new_products_section .products_wrap').slick('slickPrev');
+	});
+	$('.new_products_section .next').on('click', function() {
+	  $('.new_products_section .products_wrap').slick('slickNext');
+	});
+
+/*******/
+	$(".product_item_wrap h4.name_product").css("height", "").equalHeights();
+
+
+
+/**
+*Sort the products
+**/
   	$(".special_products .filter_switch_div li").click(function() {
 		$(".special_products .filter_switch_div li").removeClass("active");
 		$(this).addClass("active");
 	});
-
 	$("#grid_spec_products").mixItUp();
+/*****/
+
+/**
+*Скрипт запрещает перетаскивать мышью ссылки и изображение
+*The script forbids dragging links and images with the mouse
+**/
+	$("img, a").on("dragstart",function(event){
+		event.preventDefault()
+	});
 
 });
