39bbf834939dde7fa8e9d4c3f14532810077a70b
vitya752 <vitya752@ukr.net>
2018-03-15 16:24:00 +0200

    w

---
+++ mosgnb/js/common.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+	$(".header_line .info_block").css("height", "").equalHeights();
+	$(".vantage .item h4").equalHeights();
+
+	$('.slides_wrapper').slick({
+		autoplay: true,
+		autoplaySpeed: 4000,
+		dots: true,
+		speed: 1250
+	});
+
+});
