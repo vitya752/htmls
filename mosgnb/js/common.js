971d30633e2c626060313b340a1f609e9ec0aacc
vitya752 <vitya752@ukr.net>
2018-03-17 18:05:15 +0200

    s

--- mosgnb/js/common.js
+++ mosgnb/js/common.js
@@ -12,14 +12,60 @@ $(document).ready(function(){
 	});
 
 	$('.head_buttons .open_menu').click(function() {
+
 		$('.mobile_menu').slideToggle();
+
+		if( ($('.mobile_menu').is(':visible')) ){
+
+			$(this).addClass('active');
+			$('.mobile_menu').addClass('active');
+
+		}
+
 	});
 	$('.close').click(function() {
 		$('.mobile_menu').slideToggle();
+		$('.head_buttons .open_menu').removeClass('active');
+		$('.mobile_menu').removeClass('active');
 	});
 
 	$('.head_buttons .open_info').click(function() {
+
 		$('.mobile_info').slideToggle();
+
+		if( !$('.mobile_info').hasClass('active') ){
+
+			$('.head_buttons .open_info').addClass('active');
+			$('.mobile_info').addClass('active');
+
+		}else{
+
+			$('.head_buttons .open_info').removeClass('active');
+			$('.mobile_info').removeClass('active');
+
+		}
+
+	});
+
+	$('footer h3').click(function() {
+
+		var col = $(this).parent();
+		var ul = $(this).parent().find('ul');
+		var hr = $(this).parent().find('.hr');
+
+		if( col.hasClass('active') ){
+
+			col.removeClass('active');
+
+
+		}else{
+
+			col.addClass('active');
+
+		}
+		ul.slideToggle();
+		hr.slideToggle();
+
 	});
 
 });
