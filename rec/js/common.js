317b7ec9e83f297e1987e0fafe074967e9a4d1ec
vitya752 <vitya752@ukr.net>
2018-08-24 18:53:20 +0300

    s

--- rec/js/common.js
+++ rec/js/common.js
@@ -1,5 +1,24 @@
 $(function() {
 
+    $(".down").click(function () {
+        var elementClick = $(".main").next();
+        var destination = $(elementClick).offset().top;
+        $('html, body').animate({ scrollTop: destination }, 1100);
+    });
+
 	$("input, select").styler();
+
+	$(".popup").magnificPopup();
+
+	$('.faq_section ul.question li').click(function() {
+		$(this).find('.answer').slideToggle();
+
+		if( $(this).hasClass('active') ){
+			$(this).removeClass('active');
+		}else{
+			$(this).addClass('active');
+		}
+
+	});
 
 });
