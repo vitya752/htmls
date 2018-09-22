539ef65946b266c10d20ba9b0a000d1e4ab1e9df
vitya752 <vitya752@ukr.net>
2018-09-22 17:14:48 +0300

    w

--- Transfer_landing/js/common.js
+++ Transfer_landing/js/common.js
@@ -3,4 +3,9 @@ $(function() {
 	// $('input[name=phone], input[name=phone_quest]').mask('+9 (999) 999-99-99', {placeholder: "+_ (___) ___-___-__ "});	
 	$('input[name=phone], input[name=phone_quest]').mask('+9 (999) 999 99 99');
 
+	$(".tabs li").click(function(){
+		$(".tabs li").removeClass("active").eq($(this).index()).addClass("active");
+		$(".tabs_content_wrap .tab_content").hide().eq($(this).index()).fadeIn();
+	}).eq(0).addClass("active");
+
 }); 
