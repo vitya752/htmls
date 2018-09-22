6454c5d5e9ff22238d63ddfe64919ff508a742d5
vitya752 <vitya752@ukr.net>
2018-09-22 09:14:04 +0300

    w

--- Transfer_landing/js/common.js
+++ Transfer_landing/js/common.js
@@ -1,6 +1,6 @@
 $(function() {
 
 	// $('input[name=phone], input[name=phone_quest]').mask('+9 (999) 999-99-99', {placeholder: "+_ (___) ___-___-__ "});	
-	$('input[name=phone], input[name=phone_quest]').mask('+9 (999) 999-99-99');
+	$('input[name=phone], input[name=phone_quest]').mask('+9 (999) 999 99 99');
 
-});
+}); 
