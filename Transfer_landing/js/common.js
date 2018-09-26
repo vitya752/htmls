96f2f41631b6ed57fc3f932c465dbe714e29326d
vitya752 <vitya752@ukr.net>
2018-09-26 16:01:36 +0300

    w

--- Transfer_landing/js/common.js
+++ Transfer_landing/js/common.js
@@ -1,37 +1,13 @@
 $(function() {
 
 	// $('input[name=phone], input[name=phone_quest]').mask('+9 (999) 999-99-99', {placeholder: "+_ (___) ___-___-__ "});	
-	$('input[name=phone], input[name=phone_quest]').mask('+9 (999) 999 99 99');
+	$('input[name=phone], input[name=phone_quest], .phone_number').mask('+9 (999) 999 99 99');
 
 	$(".tabs li").click(function(){
 		$(".tabs li").removeClass("active").eq($(this).index()).addClass("active");
 		$(".tabs_content_wrap .tab_content").hide().eq($(this).index()).fadeIn();
 	}).eq(0).addClass("active");
 
-	// $('.slider').slick({
-	//   centerPadding: '60px',
-	//   slidesToShow: 1,
-	//   prevArrow: '<button type="button" class="slick-prev">prev</button>',
-	//   nextArrow: '<button type="button" class="slick-next">next</button>',
-	//   responsive: [
-	//     {
-	//       breakpoint: 768,
-	//       settings: {
-	//         arrows: false,
-	//         centerPadding: '40px',
-	//         slidesToShow: 1
-	//       }
-	//     },
-	//     {
-	//       breakpoint: 480,
-	//       settings: {
-	//         arrows: false,
-	//         centerPadding: '40px',
-	//         slidesToShow: 1
-	//       }
-	//     }
-	//   ]
-	// });
 
 	$("#mmenu").mmenu({
 	   "extensions": [
@@ -39,8 +15,6 @@ $(function() {
 	      "position-right"
 	   ]
 	});
-
-	$(".menu a").mPageScroll2id();
 
 	$('.slider').slick({
 	  slidesToShow: 3,
@@ -65,4 +39,48 @@ $(function() {
 		return false;
 	});
 
+	// Фиксированная шапка при скролле
+	$("header").removeClass("default");
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 20) {
+			$("header").addClass("default").fadeIn('fast');
+		} else {
+			$("header").removeClass("default").fadeIn('fast');
+		};
+	});
+
+	$(document).on("scroll", onScroll);
+	$(".menu ul a[href]").click(function(e){
+		e.preventDefault();
+		$(document).off("scroll");
+		$(menu_selector + " a.active").removeClass("active");
+		$(this).addClass("active");
+		var hash = $(this).attr("href");
+		var target = $(hash);
+		$("html, body").animate({
+		    scrollTop: target.offset().top
+		}, 1500, function(){
+			window.location.hash = hash;
+			$(document).on("scroll", onScroll);
+		});
+	});
+
+	// var phone = $('.tabs_wrap .phone_form .phone_number');
+	// formatPhone(phone);
+
 }); 
+
+var menu_selector = ".menu ul"; // Переменная должна содержать название класса или идентификатора, обертки нашего меню. 
+function onScroll(){
+	var scroll_top = $(document).scrollTop();
+	$(menu_selector + " a").each(function(){
+		var hash = $(this).attr("href");
+		var target = $(hash);
+		if (target.position().top <= scroll_top && target.position().top + target.outerHeight() > scroll_top) {
+			$(menu_selector + " a.active").removeClass("active");
+			$(this).addClass("active");
+		} else {
+			$(this).removeClass("active");
+		}
+	});
+}
