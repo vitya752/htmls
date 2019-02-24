3716edf3cf301c90f5de718bf55767864415148e
vitya752 <vitya752@ukr.net>
2019-02-24 20:25:01 +0200

    s

--- Galant/js/common.js
+++ Galant/js/common.js
@@ -1,13 +1,13 @@
 $(function() {
 
-	// $("header").removeClass("default");
-	// $(window).scroll(function(){
-	// 	if ($(this).scrollTop() > 20) {
-	// 		$("header").addClass("default").fadeIn('fast');
-	// 	} else {
-	// 		$("header").removeClass("default").fadeIn('fast');
-	// 	};
-	// });
+	$("header").removeClass("default");
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 20) {
+			$("header").addClass("default").fadeIn('fast');
+		} else {
+			$("header").removeClass("default").fadeIn('fast');
+		};
+	});
 
 	$("#menu").mmenu({
 	   "extensions": [
